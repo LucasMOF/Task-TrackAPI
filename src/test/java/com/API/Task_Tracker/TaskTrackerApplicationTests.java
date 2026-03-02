434b46146736c95e7011d20206e4d01f3fa930e2
Lucas Moura <felipelucasmoura350@gmail.com>
2026-03-02 13:41:53 -0300

    Iniciando API task-track

---
+++ src/test/java/com/API/Task_Tracker/TaskTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.API.Task_Tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
