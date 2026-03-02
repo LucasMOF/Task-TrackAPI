aae16f444861b641981a03fd88f5403177a2fdbe
Lucas Moura <felipelucasmoura350@gmail.com>
2026-03-02 14:17:27 -0300

    Criando classe Tarefa, utilizando ManyToOne, e criando tabela com chave estrangeira, ligando a classe Usuario

---
+++ src/main/java/com/API/Task_Tracker/model/Tarefa.java
@@ -0,0 +1,26 @@
+package com.API.Task_Tracker.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "tarefas")
+public class Tarefa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    private String descricao;
+    private boolean concluida = false;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+}
